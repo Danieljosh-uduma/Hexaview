4afb009efc732f93810406a7c53b1274423daa25
comfortibok <ibokcomfort93@gmail.com>
2024-12-06 09:36:14 +0100

    worked on the download page

---
+++ Download/src/download.js
@@ -0,0 +1,12 @@
+function handleDownload() {
+  const downloadButton = document.querySelector("#downloadButton");
+  downloadButton.innerHTML = "Successful";
+  downloadButton.disabled = true;
+
+  const cancelButton = document.querySelector("#cancelButton");
+  cancelButton.style.display = "none";
+
+  
+}
+const downloadButton = document.querySelector("#downloadButton");
+downloadButton.addEventListener("click", handleDownload);
